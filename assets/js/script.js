488b5f58ed451e6b843aad17709f50f9290c1eea
ddhodi <dhodisuryasayogo@gmail.com>
2025-02-19 00:47:22 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// animation navbar
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const homeLink = document.getElementById("home");
+  const brandLink = document.querySelector(".nav-home");
+
+  // Fungsi untuk menghapus semua active lalu tambah ke yang diklik
+  function setActive(link) {
+    navLinks.forEach((nav) => nav.classList.remove("active"));
+    link.classList.add("active");
+  }
+
+  // Tambahkan event listener ke setiap nav-link
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      setActive(this);
+    });
+  });
+
+  // Cek jika brandLink dan homeLink ada sebelum menambahkan event listener
+  if (brandLink && homeLink) {
+    brandLink.addEventListener("click", function () {
+      setActive(homeLink);
+    });
+  }
+});
+
